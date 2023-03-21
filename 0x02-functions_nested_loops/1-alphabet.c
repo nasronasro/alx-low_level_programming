92d1b4f564065f84f72257b52a3c1c4d080be93c
nasronasro <ahfir49481@gmail.com>
2023-03-21 14:55:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+/**
+ * main - alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	Print_alphabet();
+	return (0);
+}
+
+void Print_alphabet(void)
+{
+	int c = 97
+	int i 
+	for (i = 0; i < 25; i++)
+	{
+		putchar(c);
+	}	
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+/**
+ * main - alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	int c = 97
+	int i 
+	for (i = 0; i < 25; i++)
+	{
+		putchar(c);
+	}	
+	putchar('\n');
+}
